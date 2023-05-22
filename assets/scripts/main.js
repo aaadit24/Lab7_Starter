610c4e9b85d5a8b607ca21cb2e08dca3e9367c66
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2023-05-21 18:09:45 -0700

    Complete

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,16 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js').then((registration) => {
+          console.log('Successful:', registration);
+        })
+        .catch((err) => {
+          console.error('Failed:', err);
+        });
+    });
+  }
 }
 
 /**
@@ -68,6 +78,10 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const localRecipe = localStorage.getItem('recipes');
+  if (localRecipe) {
+    return JSON.parse(localRecipe);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -77,6 +91,19 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  const recipePromises = RECIPE_URLS.map(async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  });
+
+  const recipes = await Promise.all(recipePromises);
+  saveRecipesToStorage(recipes);
+  return recipes;
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
